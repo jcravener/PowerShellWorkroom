b3c1a40f55c7c123bbd5063c90d06b340c4124f1
john cravener <johncrav@microsoft.com>
2020-07-17 14:23:01 -0700

    working on game report

--- golf/getGameResults.ps1
+++ golf/getGameResults.ps1
@@ -18,6 +18,64 @@ $Script:jsonDepth = 20
 
 Import-Module -FullyQualifiedName $Script:modulePath -Force
 
+#--- Classes --------------------------------------------------------------------------
+
+class gameReport {
+    [int]$Score_1
+    [string]$Winner_1
+
+    [int]$Score_2
+    [string]$Winner_2
+
+    [int]$Score_3
+    [string]$Winner_3
+
+    [int]$Score_4
+    [string]$Winner_4
+
+    [int]$Score_5
+    [string]$Winner_5
+
+    [int]$Score_6
+    [string]$Winner_6
+
+    [int]$Score_7
+    [string]$Winner_7
+
+    [int]$Score_8
+    [string]$Winner_8
+
+    [int]$Score_9
+    [string]$Winner_9
+
+    [int]$Score_10
+    [string]$Winner_10
+
+    [int]$Score_11
+    [string]$Winner_11
+
+    [int]$Score_12
+    [string]$Winner_12
+
+    [int]$Score_13
+    [string]$Winner_13
+
+    [int]$Score_14
+    [string]$Winner_14
+
+    [int]$Score_15
+    [string]$Winner_15
+
+    [int]$Score_16
+    [string]$Winner_16
+
+    [int]$Score_17
+    [string]$Winner_17
+
+    [int]$Score_18
+    [string]$Winner_18
+}
+
 #--- Functions ------------------------------------------------------------------------
 function getBlindDraw {
     param (
@@ -190,14 +248,9 @@ foreach($s in $scoreTable) {
     }
 }
 
-#$scoreTable | Sort-Object -Property Team, holeNumber | Select-Object -Property *Name, Team, holeNumber, par, popCount, grossScore, netScore, netBirdie, grossBirdie, lowScore
-#$finalResults | Sort-Object -Property Hole, Team
-
 $grossBirdieTable = $scoreTable | Where-Object {$_.grossBirdie} | Group-Object -Property holeNumber, Team -NoElement | Select-Object -Property @{name = 'Hole'; expression = {($_.name -split ',')[0].trim()}}, @{name = 'Team'; expression = {($_.name -split ',')[-1].trim()}}, Count
 $netBirdieTable = $scoreTable | Where-Object {$_.netBirdie} | Group-Object -Property holeNumber, Team -NoElement | Select-Object -Property @{name = 'Hole'; expression = {($_.name -split ',')[0].trim()}}, @{name = 'Team'; expression = {($_.name -split ',')[-1].trim()}}, Count
 
-#$grossBirdieTable
-#$netBirdieTable
 foreach($r in $finalResults){
     $c = $null
     $c = ($grossBirdieTable | Where-Object -Property Hole -EQ $r.Hole | Where-Object -Property Team -EQ $r.Team).Count
@@ -218,4 +271,19 @@ foreach($r in $finalResults){
     }
 }
 
-$finalResults
+$gameReport = @{}
+
+foreach($r in $finalResults) {
+    if( -not $gameReport.ContainsKey($r.Team)){
+        $gameReport[$r.Team] = [gameReport]::new()
+    }
+    $propName = 'Score_' + $r.Hole
+    $gameReport[$r.Team].$propName = $r.combinedScore
+
+    $propName = 'Winner_' + $r.Hole
+    $gameReport[$r.Team].$propName = $r.Winner
+}
+
+foreach($k in ($gameReport.Keys | Sort-Object)) {
+    $gameReport[$k] | Select-Object -Property @{Name = 'Team'; Expression = {$k}}, *
+}
