74a117f4e4cf44f9f88f04378bbeb7a3784f8730
john cravener <johncrav@microsoft.com>
2020-07-16 22:05:08 -0700

    added team column to score record and started on script

--- golf/getGameResults.ps1
+++ golf/getGameResults.ps1
@@ -1,2 +1,13 @@
+#
 # This is the One Gross One Net score script
 #
+
+$Script:scriptRoot = Split-Path -Path $PSCommandPath -Parent
+$Script:modulePath = $Script:scriptRoot + "\module\JhcGolfScore"
+$Script:csvPath = $Script:scriptRoot + "\scores\score.csv"
+
+Import-Module -FullyQualifiedName $Script:modulePath
+
+$scoreRecord = Get-ScoreRecord -CsvFilePath $Script:csvPath
+
+$scoreRecord | New-Golfer | Get-GolferCourseHc | Get-GolferPops
